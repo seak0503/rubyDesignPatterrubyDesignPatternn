bcc5fbd35c5a33a05f23ef5db1c84883d853250d
shyamahira <shyamahira@bbtower.co.jp>
2017-05-10 00:19:26 +0900

    Factory実施中

---
+++ 11_factory_method_sample5.rb
@@ -0,0 +1,103 @@
+# Product
+class Duck
+  def initialize(name)
+    @name = name
+  end
+
+  def eat
+    puts "アヒル #{@name} は食事中です。"
+  end
+
+  def speak
+    puts "アヒル #{@name} がガーガー鳴いています。"
+  end
+
+  def sleep
+    puts "アヒル #{@name} は静かに眠っています。"
+  end
+end
+
+# Product
+class Frog
+  def initialize(name)
+    @name = name
+  end
+
+  def eat
+    puts "カエル #{@name} は食事中です。"
+  end
+
+  def speak
+    puts "カエル #{@name} はゲロゴロッと鳴いています。"
+  end
+
+  def sleep
+    puts "カエル #{@name} は静かに眠りません。一晩中ゲロゲロ鳴いています。"
+  end
+end
+
+# Product
+class Algae
+  def initialize(name)
+    @name = name
+  end
+
+  def grow
+    puts "藻 #{@name} は日光を浴びて育ちます。"
+  end
+end
+
+# Product
+class WaterLily
+  def initialize(name)
+    @name = name
+  end
+
+  def grow
+    puts "スイレン #{@name} は浮きながら日光を浴びて育ちます。"
+  end
+end
+
+# Creator
+class Pond
+  def initialize(number_animals, animal_class, number_plants, plant_class)
+    @animal_class = animal_class
+    @plant_class = plant_class
+
+    @animals = []
+    number_animals.times do |i|
+      animal = new_organism(:animal, "動物#{i}")
+      @animals << animal
+    end
+
+    @plants = []
+    number_plants.times do |i|
+      plant = new_organism(:plant, "植物#{i}")
+      @plants << plant
+    end
+  end
+
+  def simulate_one_day
+    @plants.each { |plant| plant.grow }
+    @animals.each { |animal| animal.speak }
+    @animals.each { |animal| animal.eat }
+    @animals.each { |animal| animal.sleep }
+  end
+
+  def new_organism(type, name)
+    if type == :animal
+      @animal_class.new(name)
+    elsif type == :plant
+      @plant_class.new(name)
+    else
+      raise "Unknown organism type: #{type}"
+    end
+  end
+end
+
+# ======================================
+pond = Pond.new(3, Duck, 2, WaterLily)
+pond.simulate_one_day
+
+pond = Pond.new(3, Frog, 2, Algae)
+pond.simulate_one_day
